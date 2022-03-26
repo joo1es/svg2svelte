1dfe4b1e9166e5d72c89c173e7b315d02115c59a
jooies <jooies@qq.com>
2022-03-26 10:20:52 +0800

    feat: random at one time

--- index.ts
+++ index.ts
@@ -39,23 +39,28 @@ fs.writeFileSync(`./dist/index.${answers.lang === 'typescript' ? 'ts' : 'mjs'}`,
 
 function getTemplate (name: string, svg: string) {
     // only need svg part
-    svg = svg.replace(/^.*?(<svg (.*)<\/svg>).*?$/, '$1')
-    const ids = svg.matchAll(/id=\"(.+?)\"/g)
+    svg = svg.replace(/^.*?(<svg(.*)<\/svg>).*?$/, '$1')
+    svg = svg.replace(/<style(.*)<\/style>/, '')
+    svg = svg.replace(/<script(.*)<\/script>/, '')
+    const ids = svg.matchAll(/ id=\"(.+?)\"/g)
     const data: [string, string][] = []
     let count = 0
     // id should be unique
     for (const id of ids) {
         if (!id[1]) continue
         const currentId = `id${count}`
-        svg = svg.replace(id[0], `:id="${currentId}"`)
+        svg = svg.replace(id[0], ` :id="${currentId}"`)
         svg = svg.replace(new RegExp(` ([a-z]+?)="url\\(#${id[1]}\\)"`, 'g'), ` :$1="'url(#' + ${currentId} + ')'"`)
-        data.push([currentId, `'${id[1]}_' + Math.random().toString(36).slice(-10)`])
+        data.push([currentId, `'${id[1]}_' + randomString`])
         count += 1
     }
     const dataString = count > 0 ? `
-    data: () => ({
-        ${data.map(item => `${item[0]}: ${item[1]}`).join(', ')}
-    })` : ''
+    data: () => {
+        const randomString = Math.random().toString(36).slice(-10)
+        return {
+            ${data.map(item => `${item[0]}: ${item[1]}`).join(', ')}
+        }
+    }` : ''
     return `<template>
     ${svg}
 </template>
