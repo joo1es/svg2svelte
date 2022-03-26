a4ef914a84d42f00be3aa597283ebee75f7da19f
jooies <jooies@qq.com>
2022-03-26 10:34:51 +0800

    feat: coding

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import fs from 'fs-extra'
 import inquirer from 'inquirer'
+import path from 'path'
 
 const svgs = fs.readdirSync('./src')
 if (svgs.length === 0) {
@@ -25,11 +26,13 @@ if (!fs.pathExistsSync('./dist')) {
     fs.emptyDirSync('./dist')
 }
 
-let indexArray: string[] = []
+const indexArray: string[] = []
 
 for (const svg of svgs) {
-    if (!svg.endsWith('.svg')) continue
-    const name = toUpperCamelCase(svg.replace('.svg', ''))
+    const { ext } = path.parse(svg)
+    if (ext !== '.svg') continue
+
+    const name = toUpperCamelCase(svg.replace(/\.svg$/i, ''))
     const template = getTemplate(name, fs.readFileSync(`./src/${svg}`, 'utf8'))
     fs.writeFileSync(`./dist/${name}.vue`, template)
     indexArray.push(`export { default as ${name} } from './${name}.vue'`)
@@ -39,21 +42,27 @@ fs.writeFileSync(`./dist/index.${answers.lang === 'typescript' ? 'ts' : 'mjs'}`,
 
 function getTemplate (name: string, svg: string) {
     // only need svg part
-    svg = svg.replace(/^.*?(<svg(.*)<\/svg>).*?$/, '$1')
-    svg = svg.replace(/<style(.*)<\/style>/, '')
-    svg = svg.replace(/<script(.*)<\/script>/, '')
+    svg = svg
+        .replace(/^.*?(<svg(.*)<\/svg>).*?$/, '$1')
+        .replace(/<style(.*)<\/style>/, '')
+        .replace(/<script(.*)<\/script>/, '')
+
     const ids = svg.matchAll(/ id=\"(.+?)\"/g)
+
     const data: [string, string][] = []
     let count = 0
+
     // id should be unique
     for (const id of ids) {
         if (!id[1]) continue
         const currentId = `id${count}`
-        svg = svg.replace(id[0], ` :id="${currentId}"`)
-        svg = svg.replace(new RegExp(` ([a-z]+?)="url\\(#${id[1]}\\)"`, 'g'), ` :$1="'url(#' + ${currentId} + ')'"`)
+        svg = svg
+            .replace(id[0], ` :id="${currentId}"`)
+            .replace(new RegExp(` ([a-z]+?)="url\\(#${id[1]}\\)"`, 'g'), ` :$1="'url(#' + ${currentId} + ')'"`)
         data.push([currentId, `'${id[1]}_' + randomString`])
         count += 1
     }
+
     const dataString = count > 0 ? `
     data: () => {
         const randomString = Math.random().toString(36).slice(-10)
@@ -61,6 +70,7 @@ function getTemplate (name: string, svg: string) {
             ${data.map(item => `${item[0]}: ${item[1]}`).join(', ')}
         }
     }` : ''
+
     return `<template>
     ${svg}
 </template>
@@ -71,7 +81,8 @@ import { defineComponent } from 'vue'
 export default defineComponent({
     name: '${name}',${dataString}
 })
-</script>`
+</script>
+`
 }
 
 function toUpperCamelCase (str: string) {
